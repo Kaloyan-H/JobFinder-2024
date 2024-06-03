f47cdb52e5815965155616d581664b38774c1c70
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-03 18:27:11 +0300

    Created DecimalModelBinder and  DecimalModelBinderProvider.

---
+++ JobFinder/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace JobFinder.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                decimal result = 0M;
+                bool successFlag = false;
+
+                try
+                {
+                    string stringValue = valueResult.FirstValue.Trim();
+
+                    stringValue = stringValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    stringValue = stringValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    result = Convert.ToDecimal(stringValue, CultureInfo.CurrentCulture);
+                    successFlag = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (successFlag)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(result);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
