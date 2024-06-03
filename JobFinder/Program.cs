05f84a37aa3e2db6793f2714d4df18699f8196ed
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-03 18:28:02 +0300

    Added DecimalModelBinderProvider to ModelBinderProviders.

--- JobFinder/Program.cs
+++ JobFinder/Program.cs
@@ -1,9 +1,15 @@
+using JobFinder.ModelBinders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options  =>
+    {
+        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+    });
 
 builder.Services.AddApplicationServices();
 
