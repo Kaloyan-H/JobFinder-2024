da85f8518677f22e7f7dea436e1746e354db61ff
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-07 16:43:59 +0300

    Some password requirements removed.

--- JobFinder/Extensions/ServiceCollectionExtension.cs
+++ JobFinder/Extensions/ServiceCollectionExtension.cs
@@ -28,6 +28,10 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddIdentity<AppUser, AppRole>(options =>
                 {
                     options.SignIn.RequireConfirmedAccount = true;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
                 })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             
