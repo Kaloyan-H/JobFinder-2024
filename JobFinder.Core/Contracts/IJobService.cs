ef1bee727084f262fb5961c31fb05cc6fbf66f65
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-10 21:29:03 +0300

    JobService created.

---
+++ JobFinder.Core/Contracts/IJobService.cs
@@ -0,0 +1,17 @@
+﻿using JobFinder.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobFinder.Core.Contracts
+{
+    public interface IJobService
+    {
+        public Task<bool> ExistsAsync(int jobId);
+
+        public Task<bool> CreateAsync();
+    }
+}
---
+++ JobFinder.Core/Services/JobService.cs
@@ -0,0 +1,29 @@
+﻿using JobFinder.Core.Contracts;
+using JobFinder.Infrastructure.Common;
+using JobFinder.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobFinder.Core.Services
+{
+    public class JobService : IJobService
+    {
+        private IRepository repository;
+
+        public JobService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<bool> ExistsAsync(int jobId)
+        {
+            return await repository.AllReadOnly<Job>()
+                .AnyAsync(j => j.Id == jobId);
+        }
+
+        public async Task<bool> CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
