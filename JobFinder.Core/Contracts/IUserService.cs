ee5141b41635f45c3f26f6583632439f54002191
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-18 17:28:06 +0300

    UserService created.

---
+++ JobFinder.Core/Contracts/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace JobFinder.Core.Contracts
+{
+    public interface IUserService
+    {
+        public Task<bool> HasCompanyAsync(string userId);
+    }
+}
---
+++ JobFinder.Core/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using JobFinder.Core.Contracts;
+using JobFinder.Infrastructure.Common;
+using JobFinder.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobFinder.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository repository;
+
+        public UserService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<bool> HasCompanyAsync(string userId)
+        {
+            var user = await repository.All<AppUser>()
+                .FirstOrDefaultAsync(au => au.Id == userId);
+
+            return user?.Company != null;
+        }
+    }
+}
