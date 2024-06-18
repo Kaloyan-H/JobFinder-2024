cc35c6cc341cf89543a0db7e6ed7b7f902cd0ffe
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-18 16:17:11 +0300

    HasCompany attribute created.

---
+++ JobFinder/Attributes/HasCompanyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using JobFinder.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace JobFinder.Attributes
+{
+    public class HasCompanyAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var user = context.HttpContext.User;
+            IUserService? userService = context.HttpContext.RequestServices.GetService<IUserService>();
+
+            if (userService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (await userService!.HasCompanyAsync(user.Id()))
+            {
+                context.Result = new RedirectToActionResult("Company", "Create", null);
+            }
+
+            await next();
+        }
+    }
+}
