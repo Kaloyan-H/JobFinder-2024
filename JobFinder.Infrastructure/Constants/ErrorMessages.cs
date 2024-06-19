a0a120d2a97080f7fa8b8c39181f6ac6f3c726c6
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-19 22:48:41 +0300

    Error messages created.

---
+++ JobFinder.Infrastructure/Constants/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace JobFinder.Infrastructure.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string RequiredErrorMessage = "The field {0} is required.";
+        public const string MinSalaryHigherErrorMessage = "Minimum salary should be lower than maximum salary";
+        public const string RoleDoesNotExistErrorMessage = "Role does not exist.";
+        public const string CategoryDoesNotExistErrorMessage = "Category does not exist.";
+        public const string EmploymentTypeDoesNotExistErrorMessage = "Employment type does not exist.";
+    }
+}
