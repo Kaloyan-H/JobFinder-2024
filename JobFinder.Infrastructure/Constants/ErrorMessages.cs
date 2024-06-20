e3e0a2690d774569927c22cd9a663e7b5db3696e
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-20 14:11:16 +0300

    Error message added.

--- JobFinder.Infrastructure/Constants/ErrorMessages.cs
+++ JobFinder.Infrastructure/Constants/ErrorMessages.cs
@@ -7,5 +7,6 @@
         public const string RoleDoesNotExistErrorMessage = "Role does not exist.";
         public const string CategoryDoesNotExistErrorMessage = "Category does not exist.";
         public const string EmploymentTypeDoesNotExistErrorMessage = "Employment type does not exist.";
+        public const string UserDoesNotExistErrorMessage = "User does not exist.";
     }
 }
