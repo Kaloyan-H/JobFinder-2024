d50d5f85dd523904099b75e7dfb97519efe02997
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-06-29 18:18:21 +0300

    Error message changed.

--- JobFinder.Infrastructure/Constants/ErrorMessages.cs
+++ JobFinder.Infrastructure/Constants/ErrorMessages.cs
@@ -3,7 +3,7 @@
     public static class ErrorMessages
     {
         public const string RequiredErrorMessage = "The field {0} is required.";
-        public const string MinSalaryHigherErrorMessage = "Minimum salary should be lower than maximum salary";
+        public const string MinSalaryHigherErrorMessage = "Minimum salary should be lower or equal to maximum salary";
         public const string RoleDoesNotExistErrorMessage = "Role does not exist.";
         public const string CategoryDoesNotExistErrorMessage = "Category does not exist.";
         public const string EmploymentTypeDoesNotExistErrorMessage = "Employment type does not exist.";
