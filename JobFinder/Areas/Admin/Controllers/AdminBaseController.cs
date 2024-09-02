a976aa6c0cafa8d23ce4dc07b0f2428eaaceb018
Kaloyan-H <105971664+Kaloyan-H@users.noreply.github.com>
2024-09-02 20:12:14 +0300

    AreaConstants created.

--- JobFinder/Areas/Admin/Controllers/AdminBaseController.cs
+++ JobFinder/Areas/Admin/Controllers/AdminBaseController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static JobFinder.Constants.AreaConstants;
 using static JobFinder.Infrastructure.Constants.RoleConstants;
 
 namespace JobFinder.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area(AdminAreaName)]
     [Authorize(Roles = ADMINISTRATOR_ROLE)]
     public class AdminBaseController : Controller
     { }
---
+++ JobFinder/Constants/AreaConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace JobFinder.Constants
+{
+    public static class AreaConstants
+    {
+        public const string AdminAreaName = "Admin";
+    }
+}
